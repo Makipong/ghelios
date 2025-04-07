65486631140a497d757d4d3d66146bae0be98199
Makipong <140858041+Makipong@users.noreply.github.com>
2025-04-07 23:29:58 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { 
+    unoptimized: true,
+    domains: ['images.unsplash.com']
+  }
+};
+
+module.exports = withPWA(nextConfig);
